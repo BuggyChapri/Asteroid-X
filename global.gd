f640bde510d29725dcc6b7f4f9e580b2b93ba2d9
A Boring github profile¯⁠\⁠_⁠(⁠ ͡⁠°⁠ ͜⁠ʖ⁠ ͡⁠°⁠)⁠_⁠/⁠¯ <darkkboy0013@gmail.com>
2024-12-10 10:21:54 +0530

    Add files via upload

---
+++ global.gd
@@ -0,0 +1,10 @@
+extends Node
+
+var sp_enemy = false
+var score = 0
+var damage_astro_small = 40
+var damage_astro_big = 30
+var damage_astro_very_big = 25
+var enemy_damage = 20
+var damage_boss = 20
+var boss_health = 6000
